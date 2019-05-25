6dd69f52d66aaf0f1bb03afd43e5c4838829b6ed
sirjan13CN <sirjanpreet.singh@codenation.co.in>
2019-05-26 02:21:52 +0530

    Updated allowed_hosts

--- cn_django_tut/settings.py
+++ cn_django_tut/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aesayji4nr)g(-$(g9aw5q5myao#yjcdruj&ebc8zw)$zd3g5-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://k70830f4d7e04432ba05f469be96d7b49-8000.cy.devfactory.com']
+ALLOWED_HOSTS = ['sirjan.cndjango.cy.devfactory.com']
 
 
 # Application definition
